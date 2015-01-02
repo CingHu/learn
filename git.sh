5006dad100f582d582fb177c4867140fc88f44c7
CingHu <yige2008123@126.com>
2015-01-02 10:06:32 +0800

    git code commit

---
+++ git.sh
@@ -0,0 +1,103 @@
+
+#git remote add 命令把它加为当前项目的远程分支之一。我们把它命名为 teamone，以便代替原始的 Git 地址
+git fetch teamone
+#现在你可以用 git fetch teamone 来获取小组服务器上你还没有的数据了。由于当前该服务器上的内容是你 origin 服务器上的子集，Git 不会下载任何数据，而只是简单地创建一个名为teamone/master 的分支，指向 teamone 服务器上 master分支所在的提交对象31b8e,你在本地有了一个指向 teamone 服务器上 master 分支的索引
+
+
+
+git push (远程仓库名) (分支名)         //推送本地分支, 例如：git push origin serverfix
+
+git push origin serverfix:serferfix   //上传我本地的 serverfix 分支到远程仓库中去，仍旧称它为 serverfix 分支
+
+git push origin serverfix:awesomebranch  //若想把远程分支叫作awesomebranch，可以用 git push origin serverfix:awesomebranch 来推送数据
+
+git fetch origin                         //将得到一个新的远程分支 origin/serverfix
+
+git merge origin/serverfix               //把内容合并到当前分支，这个分支是一个远程分支
+
+git checkout -b serverfix origin/serverfix  //创建一个自己的serverfix开发
+
+git checkout --track origin/serverfix    //跟踪远程分支
+
+git checkout -b sf origin/serverfix      //现在你的本地分支 sf 会自动向 origin/serverfix 推送和抓取数据了
+
+git push origin :serverfix               //删除远程分支
+
+
+git checkout experiment ===>  git rebase master 
+
+git rebase --onto master server client ===> git checkout master ==> git merge client ==>  git rebase master server ==> git checkout master ==> git merge server ==> git branch -d client ==> git branch -d server  //我们就可以把基于 server 分支而非 master 分支的改变（即 C8 和 C9），跳过 server 直接放到master 分支中重演一遍，但这需要用 git rebase 的 –onto 选项指定新的基底分支master
+
+git diff --check                      //提交之前，检查空白字符
+
+git log --no-merges                  //还没有merge的分支
+
+
+git fetch origin  ===> git merge origin/master ==> git push origin master       //提交之前需要fetch仓库里最新的代码,同时将代码合并到最新,然后在push代码
+
+
+git log --no-merges origin/master ^issue54 //查看issue54本地分支与当前最新的仓库代码的区别
+
+
+git fetch origin ==> git log --no-merges origin/master ^issue54 ==> git checkout master ==> git merge issue54 ==> git merge origin/master ==> git push origin master   //完整的更新代码的流程
+
+//user1与user2一起开发feature这个特性
+git checkout -b featureA ==> git push origin featureA  //user1开发特性A完成
+git fetch origin ==> git checkout -b featureB origin/master ==> git commit -am 'add ls-files' ==> git fetch origin ==> git merge origin/featureBee ==> git push origin featureB:featureBee ==> git log origin/featureA ^featureA ==> git checkout featureA ==> git merge origin/featureA ==> git commit -am 'small tweak' ==> git push origin featureA   //user2开发过程中user1已经更新了代码并提交到仓库里了，所以user2在提交之前需要再次获取到新的代码并合并到最新，然后在将代码推送的featureB分支
+
+
+git apply --check 0001-seeing-if-this-helps-the-gem.patch  //查看补丁是否能够干净顺利地应用到当前分支中
+git apply /tmp/patch-ruby-client.patc   //应用补丁
+
+git shortlog --no-merges master --not v1.0.1   //统计给定范围内的所有提交;假如你上一次发布的版本是v1.0.1，下面的命令将给出自从上次发布之后的所有提交的简介
+
+git reflog 734713b  //引用日志
+
+git show HEAD@{5}   //如果你想查看仓库中 HEAD 在五次前的值，你可以使用引用日志的输出中的@{n}
+
+git show master@{yesterday}  //查看一定时间前分支指向哪里
+
+git show HEAD@{2.months.ago}  //2个月之前的提交
+
+git show HEAD^  //查看HEAD 的父提交
+git show HEAD^2  //查看HEAD第二的父提交
+git show HEAD~3  //不太清楚，看着用
+
+
+
+git config --global core.editor vim
+
+git config --global core.pager less //core.pager指定 Git 运行诸如log、diff等所使用的分页器
+
+git config --global core.excludesfile //提交时，忽略的文件定义
+
+
+git config --global help.autocorrect //命令自动修正
+
+
+git config --global color.ui true     //git着色, 可选择false, always,与着色有关的选项color.branch color.diff color.interactive color.status
+
+git config --global core.autocrlf true //提交时自动地把行结束符CRLF转换成LF，而在签出代码时把LF转换成CRLF
+git config --global core.autocrlf input  //Linux或Mac系统使用LF作为行结束符，因此你不想 Git 在签出文件时进行自动的转换；当一个以CRLF为行结束符的文件不小心被引入时你肯定想进行修正，把core.autocrlf设置成input来告诉 Git 在提交时把CRLF转换成LF，签出时不转换
+git config --global core.autocrlf false //如果你是Windows程序员，且正在开发仅运行在Windows上的项目，可以设置false取消此功能，把回车符记录在库中
+
+git config --global core.whitespace //trailing-space会查找每行结尾的空格,space-before-tab会查找每行开头的制表符前的空格,indent-with-non-tab会查找8个以上空格（非制表符）开头的行,cr-at-eol让 Git 知道行尾回车符是合法的
+git config --global core.whitespace \ trailing-space,space-before-tab,indent-with-non-tab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ repo/suse12.3.sh
@@ -0,0 +1,4 @@
+ zypper addrepo -f http://ftp.sjtu.edu.cn/opensuse/distribution/12.3/repo/non-oss/ sjtu.openSUSE-12.3-Non-Oss
+zypper addrepo -f http://ftp.sjtu.edu.cn/opensuse/distribution/12.3/repo/oss/ sjtu.openSUSE-12.3-Oss
+zypper addrepo -f http://ftp.sjtu.edu.cn/opensuse/update/12.3/ sjtu.openSUSE-12.3-Update
+zypper addrepo -f http://ftp.sjtu.edu.cn/opensuse/update/12.3-non-oss/ sjtu.openSUSE-12.3-Update-Non-Oss
