5c1bb04b856c2082df12001ce857b290cf9bfd47
cinghu <yige2008123@126.com>
2020-01-10 10:51:27 +0800

    add script at ct

--- ct/collect_ovs_log.sh
+++ ct/collect_ovs_log.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 
-
 #ovs-appctl vlog/set any:any:dbg
 #ovs-appctl vlog/set any:any:info
-
 #/usr/bin/dpdk-pdump -d librte_pmd_pcap.so -- --pdump port=0,queue=*,rx-dev=/tmp/hupkts.pcap,tx-dev=/tmp/hupkts.pcap --server-socket-path=/var/run/openvswitch
 #ovs-vsctl clear Bridge br0 mirror
 
@@ -12,20 +10,17 @@
 mkdir ovslog > /dev/null
 date=$(date "+%Y-%m-%d %H:%M:%S" | sed 's/\ /_/g')
 file=ovslog/ovs-$date.log
-
 >$file
 function run_cmd(){
     echo "$*" >> $file
     $* >> $file
     echo -e "===========$* End========\n\n\n" >> $file
 }
-
 for bridge in  `ovs-vsctl list bridge | grep name | cut -d":" -f2`
 do
     run_cmd ovs-ofctl dump-flows ${bridge}
     run_cmd ovs-ofctl dump-ports ${bridge}
 done
-
 run_cmd ovs-vsctl show
 run_cmd ovs-appctl dpctl/dump-flows
 run_cmd ovs-appctl dpctl/dump-conntrack
@@ -36,16 +31,15 @@ run_cmd ifconfig -a
 run_cmd ovsdb-tool show-log -mmm
 run_cmd ovs-appctl  dpif-netdev/pmd-stats-show
 run_cmd ovs-appctl dpif-netdev/pmd-rxq-show
-
 for ns in `ip netns list | awk '{print $1}'`
 do
     run_cmd ip netns exec $ns ip a
     run_cmd ip netns exec $ns ip route
     run_cmd ip netns exec $ns ip neigh
 done
-
 run_cmd dmesg
 run_cmd dmesg -T
-
 run_cmd  cp /var/log/openvswitch/ovsdb-server.log ovslog/ovsdb-server.log-$date.log
 run_cmd  cp /var/log/messages   ovslog/messages-$date.log
+
+
--- ct/l2gateway-demo-flow.sh
+++ ct/l2gateway-demo-flow.sh
@@ -62,40 +62,42 @@ ovs-ofctl del-flows $BASEBRIDGE
 ovs-ofctl del-flows $BRIDGE
 ovs-ofctl del-flows $TUNBRIDGE
 
-ovs-ofctl add-flow $TUNBRIDGE  "table=0,  in_port=vxlan0, tun_id=$vni actions=output:${patchtunp}"
-ovs-ofctl add-flow $TUNBRIDGE  "table=0,  in_port=$patchtunp, actions=resubmit(,1)"
-ovs-ofctl add-flow $TUNBRIDGE  "table=1,  dl_dst=$vmmac  actions=set_field:$vni->tun_id, set_field:$localip->tun_src,set_field:$tunnelip->tun_dst,output:vxlan0"
-ovs-ofctl add-flow $TUNBRIDGE  "table=1,  dl_dst=$gwmac,arp  actions=set_field:$vni->tun_id, set_field:$localip->tun_src,set_field:$tunnelip->tun_dst,output:vxlan0"
+ovs-ofctl add-flow $TUNBRIDGE  "table=0,  priority=100,in_port=vxlan0, tun_id=$vni actions=output:${patchtunp}"
+ovs-ofctl add-flow $TUNBRIDGE  "table=0,  priority=1 actions=resubmit(,1)"
+ovs-ofctl add-flow $TUNBRIDGE  "table=1,  priority=100,dl_dst=$vmmac  actions=set_field:$vni->tun_id, set_field:$localip->tun_src,set_field:$tunnelip->tun_dst,output:vxlan0"
+ovs-ofctl add-flow $TUNBRIDGE  "table=1,  priority=100,dl_dst=$gwmac,arp  actions=set_field:$vni->tun_id, set_field:$localip->tun_src,resubmit(,2)"
+ovs-ofctl add-flow $TUNBRIDGE  "table=1,  priority=1 actions=drop"
+ovs-ofctl add-flow $TUNBRIDGE  "table=2,  priority=100,tun_id=$vni  actions=set_field:$tunnelip->tun_dst,output:vxlan0"
+ovs-ofctl add-flow $TUNBRIDGE  "table=2,  priority=1 actions=drop"
+
 #ovs-ofctl add-flow $TUNBRIDGE  "table=1,  dl_dst=$tunnelhstmac  actions=set_field:$gwmac->eth_dst,set_field:$vni->tun_id, set_field:$localip->tun_src,set_field:$tunnelip->tun_dst,output:vxlan0"
 
-ovs-ofctl add-flow $BASEBRIDGE  "table=0,  in_port=bond1,dl_vlan=$vlan actions=strip_vlan,output:${patchp}"
-ovs-ofctl add-flow $BASEBRIDGE  "table=0, in_port=$patchp actions=mod_vlan_vid:$vlan,output:bond1"
+ovs-ofctl add-flow $BASEBRIDGE  "table=0, priority=100, in_port=bond1,dl_vlan=$vlan actions=strip_vlan,output:${patchp}"
+ovs-ofctl add-flow $BASEBRIDGE  "table=0, priority=100,in_port=$patchp actions=mod_vlan_vid:$vlan,output:bond1"
+ovs-ofctl add-flow $BASEBRIDGE  "table=0, priority=1,in_port=$patchp actions=drop"
 
-$ADDFLOW "table=0,  in_port=$patchl actions=resubmit(,10)"
-$ADDFLOW "table=0, priority=1000,in_port=$patchtunl actions=resubmit(,40)"
+$ADDFLOW "table=0, priority=100 in_port=$patchl actions=resubmit(,10)"
+$ADDFLOW "table=0, priority=100,in_port=$patchtunl actions=resubmit(,40)"
 $ADDFLOW "table=0, priority=1 actions=mod_vlan_vid:1,resubmit(,100)"
 
 
-$ADDFLOW "table=10, priority=2000,arp actions=resubmit(,15)"
-$ADDFLOW "table=10, priority=1000,ip, nw_dst=$vmip  actions=mod_dl_dst:$vmmac,resubmit(,20)"
-$ADDFLOW "table=10, priority=1000,ip, nw_dst=$gwip  actions=mod_dl_dst:$gwmac,resubmit(,20)"
-#$ADDFLOW "table=10, priority=1000,ip, nw_dst=$gwip  actions=mod_dl_dst:$tunnelhstmac,resubmit(,20)"
+$ADDFLOW "table=10, priority=200,arp actions=resubmit(,15)"
+$ADDFLOW "table=10, priority=100,ip, nw_dst=$vmip  actions=mod_dl_dst:$vmmac,resubmit(,20)"
+$ADDFLOW "table=10, priority=100,ip, nw_dst=$gwip  actions=mod_dl_dst:$gwmac,resubmit(,20)"
 $ADDFLOW "table=10, priority=1 actions=resubmit(,20)"
 
-$ADDFLOW "table=15,priority=1000,arp,arp_op=1, arp_tpa=$vmip actions=load:0x2->NXM_OF_ARP_OP[],move:NXM_NX_ARP_SHA[]->NXM_NX_ARP_THA[],move:NXM_OF_ARP_SPA[]->NXM_OF_ARP_TPA[],load:$hostmachex->NXM_NX_ARP_SHA[],load:$vmiphex->NXM_OF_ARP_SPA[],move:NXM_OF_ETH_SRC[]->NXM_OF_ETH_DST[],mod_dl_src:$hostmac,IN_PORT"
-$ADDFLOW "table=15,priority=1000,arp,arp_op=2, arp_tpa=$vmip actions=load:$vmmachex->NXM_NX_ARP_THA[], mod_dl_dst:$vmmac, resubmit(,20)"
-#$ADDFLOW "table=15,priority=1000,arp,arp_op=2, arp_tpa=$gwip actions=load:$tunnelhstmachex->NXM_NX_ARP_THA[], mod_dl_dst:$tunnelhstmac, resubmit(,20)"
-$ADDFLOW "table=15,priority=1000,arp,arp_op=2, arp_tpa=$gwip actions=load:$tunnelhstmac->NXM_NX_ARP_THA[], mod_dl_dst:$gwmac, resubmit(,20)"
-$ADDFLOW "table=15, priority=1 actions=resubmit(,20)"
+$ADDFLOW "table=15,priority=100,arp,arp_op=1, arp_tpa=$vmip actions=load:0x2->NXM_OF_ARP_OP[],move:NXM_NX_ARP_SHA[]->NXM_NX_ARP_THA[],move:NXM_OF_ARP_SPA[]->NXM_OF_ARP_TPA[],load:$hostmachex->NXM_NX_ARP_SHA[],load:$vmiphex->NXM_OF_ARP_SPA[],move:NXM_OF_ETH_SRC[]->NXM_OF_ETH_DST[],mod_dl_src:$hostmac,IN_PORT"
+$ADDFLOW "table=15,priority=100,arp,arp_op=2, arp_tpa=$vmip actions=load:$vmmachex->NXM_NX_ARP_THA[], mod_dl_dst:$vmmac, resubmit(,20)"
+$ADDFLOW "table=15,priority=100,arp,arp_op=2, arp_tpa=$gwip actions=load:$gwmachex->NXM_NX_ARP_THA[], mod_dl_dst:$gwmac, resubmit(,20)"
 
 $ADDFLOW "table=20, priority=1 actions=strip_vlan,output:$patchtunl"
 
-$ADDFLOW "table=40, priority=1000,arp actions=resubmit(,45)"
-$ADDFLOW "table=40, priority=100,ip actions=mod_dl_src:$hostmac,output:$patchl"
+$ADDFLOW "table=40, priority=100,arp actions=resubmit(,45)"
+$ADDFLOW "table=40, priority=10,ip actions=mod_dl_src:$hostmac,output:$patchl"
 $ADDFLOW "table=40, priority=1 actions=mod_vlan_vid:40,resubmit(,100)"
 
 
-$ADDFLOW "table=45,priority=1000,arp,arp_op=1 actions=load:$hostmachex->NXM_NX_ARP_SHA[], mod_dl_src:$hostmac, output:$patchl"
+$ADDFLOW "table=45,priority=100,arp,arp_op=1 actions=load:$hostmachex->NXM_NX_ARP_SHA[], mod_dl_src:$hostmac, output:$patchl"
 $ADDFLOW "table=45, priority=1 actions=mod_vlan_vid:45,resubmit(,100)"
 
 $ADDFLOW "table=100, priority=0 actions=drop"
